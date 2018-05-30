152e4c37e3da1bd17a802ad8c3f71a63d86db51c
Iona Macbeth <ionalmacbeth@gmail.com>
2018-05-30 11:26:39 +0100

    initial commit

---
+++ specs/drink_spec.rb
@@ -0,0 +1,13 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../drink.rb")
+
+class DrinkTest < MiniTest::Test
+
+  def setup
+    @name = name
+    @price = price
+  end
+
+
+end
